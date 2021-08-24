d4e9ca9fcfd860dcfa02f39034c87bdf8395319c
rahat <mirrahat00@gmail.com>
2021-08-24 17:06:10 +0600

    home section and footer added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 import { Navber } from './components/Navber';
 import { SectionOne } from './components/SectionOne';
@@ -10,6 +11,7 @@ function App() {
       <Navber />
       <Header />
       <SectionOne />
+      <Footer />
       
     </div>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from "../assets/footer.module.css";
+
+export const Footer = () => {
+  return (
+    <>
+      <div className={styles.footer}>
+        <div className={styles.secOne}>
+          <img
+            src="https://surokkha.gov.bd/static/media/logo_white.8ef9a1c4.svg"
+            alt="pic"
+          />
+        </div>
+        <div className={styles.secTwo}>
+          <ul>
+            <li><a className={styles.links}  href="#">Privacy Policy</a></li>
+            <li><a className={styles.links} href="#">Terms of service</a></li>
+            <li><a className={styles.links} href="#">Other Affiliates</a></li>
+          </ul>
+        </div>
+        <div className={styles.secThree}>
+          <p>Developed by - rahat</p>
+          <img
+            src="https://surokkha.gov.bd/static/media/ict.52410752.png"
+            alt="imgss"
+          />
+        </div>
+        <div className={styles.secFour}>
+          <p>Affiliates -</p>
+          <img
+            src="https://surokkha.gov.bd/static/media/org.ed08c793.png"
+            alt="imgss"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
