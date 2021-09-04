8ff192cdf526321588f7b696f7469daf30a1bc3e
rahat <201853082050@nuist.edu.cn>
2021-09-04 22:33:23 +0600

    Registration page modification complete

--- cal.js
+++ cal.js
@@ -1,3 +1,9 @@
-const data = Math.random() * 10;
-let newdata = data.toFixed()
-console.log(newdata);
+// const data = Math.random() * 10;
+// let newdata = data.toFixed()
+// console.log(newdata);
+
+let i = 1;
+while (i >30) {
+    console.log(i)
+    i++;
+}
